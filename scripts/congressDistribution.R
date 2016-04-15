1b21115f5568dea87e196934859ddcc8e0874d98
Ryan Tuggle <ryan.tuggle@gmail.com>
2016-04-15 13:58:16 -0400

    adding scripts directory and a few scripts for loading data and congressional analysis

---
+++ scripts/congressDistribution.R
@@ -0,0 +1,67 @@
+##prep data for congressional analysis
+require(dplyr)
+
+#create a unique key for awards and districts, select needed variables
+df <- fpds %>%
+    mutate(uniqueId = paste(PIID.Agency.ID, PIID, Referenced.IDV.Agency.ID, 
+                            Referenced..IDV.PIID, sep='-'),
+           congressId = paste(Principal.Place.of.Performance.State.Code,
+                              Congressional.District.Place.of..Performance,
+                              sep='-')) %>%
+    select(uniqueId, congressId, Action.Obligation,
+           Funding.Department.Name, PIID.Agency.ID, PIID, Referenced.IDV.Agency.ID, 
+           Referenced..IDV.PIID, Modification.Number, 
+           Principal.Place.of.Performance.State.Code,
+           Congressional.District.Place.of..Performance) 
+
+#for reference count how many distinct unique ids
+cntAwards <- n_distinct(df$uniqueId)
+paste('the number of distinct awards in dataset is:', 
+      prettyNum(cntAwards, big.mark = ',', scientific = FALSE))
+
+#remove records with NA in CongressId
+noNa <- df %>%
+    filter(!grepl("NA", congressId))
+
+#compare the full and noNa datasets (TASK: bind together to make table)
+summarize(df, distinct = n_distinct(uniqueId), dollars = sum(Action.Obligation))
+summarize(noNa, distinct = n_distinct(uniqueId), dollars = sum(Action.Obligation))
+
+#look for remaining records with more than one district
+#this is not meant to happen according to data model
+test <- noNa %>%
+    group_by(uniqueId) %>%
+    summarize(districts = n_distinct(congressId),
+              list = paste(congressId, 
+                           collapse = "|")) %>%
+    filter(districts > 1) %>%
+    arrange(desc(districts))
+
+#write the errors out for examination
+write.csv(test, file = "~/Repositories/data/exceptionsCongress.csv", row.names = FALSE)
+
+#get the total spending by Funding Department, rank, and get percentages
+chart <- noNa %>%
+    group_by(Funding.Department.Name, congressId) %>%
+    summarize(total = sum(Action.Obligation)) %>%
+    arrange(Funding.Department.Name, desc(total)) %>%
+    group_by(Funding.Department.Name) %>%
+    mutate(rank = row_number(), pctFund = (cumsum(total) / sum(total)) * 100, 
+           #pctDistrict = cumsum(rank) / sum(rank))
+        pctDistrict = cumsum(rank) / 435)
+
+#chart the results
+ggplot(data = filter(chart, grepl("DEPARTMENT",Funding.Department.Name)),
+       aes(x = pctDistrict, y = pctFund, 
+           group = Funding.Department.Name,
+           colour = Funding.Department.Name)) +
+    geom_line() +
+    scale_x_log10()
+
+
+ggplot(data = filter(chart, !grepl("DEPARTMENT",Funding.Department.Name)),
+       aes(x = pctDistrict, y = pctFund, 
+           group = Funding.Department.Name,
+           colour = Funding.Department.Name)) +
+    geom_line()
+    
---
+++ scripts/loadData.R
@@ -0,0 +1,22 @@
+##script to create data frame of fpds data
+#define the directory location of the data files 
+datadir <- '~/Repositories/data/FPDS_20160414'
+
+#create function that takes the filename and adds it to the dataset
+#will use this function in do.call below
+read.helper <- function(infile,datadir,...){
+    ## Function to read in csv and strip date information from file name
+    temp <- read.csv(paste(datadir,infile,sep="/"), stringsAsFactors = F,...)
+    temp$period <- gsub("FPDS","",infile) %>% gsub("\\.csv","",.)
+    return(temp)
+}
+
+#get a list of files in the data directory
+files <- list.files(datadir,pattern = "FPDSFULL[0-9_]*.csv")
+
+#rowbind all the files together, with the added filename in the dataset
+fpds <- do.call(rbind, lapply(files, function(x) read.helper(x,datadir)))
+
+#strip the dollars from Action.Obligation
+fpds$Action.Obligation <- gsub("[^0-9.]","",fpds$Action.Obligation) %>% 
+    as.numeric()
