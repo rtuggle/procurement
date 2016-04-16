5f7d324bc93813c7a2ad12cf4f904f4f8efedcb6
J.B <jessica.k.bonnie@gmail.com>
2016-04-15 20:42:03 -0400

    Added some approaches to AUC

---
+++ scripts/workAuc_JB.R
@@ -0,0 +1,36 @@
+## packages
+require(MESS)
+require(zoo)
+require(caTools)
+
+require(Bolstad2)
+
+###NOTE : Use the vendorDistribution Chart variable as produced after repairs
+
+###Note : There is no curve to calculate AUC if there is only one point or the value is NA.
+
+
+chart.test1 <- chart %>% filter(!is.nan(pctFund))
+
+
+## Calculate using trapezoid method (less desired according to SO)
+
+thing.trap <- chart %>% ungroup () %>%
+  group_by(Funding.Department.Name) %>%
+  summarize(test=caTools::trapz(pctVendor,pctFund)) 
+
+
+#Calculate using Spline Method
+
+thing.spline <- chart.test1 %>% ungroup () %>%
+  group_by(Funding.Department.Name) %>%
+  summarize(test=MESS::auc(pctVendor,pctFund,type = 'spline'))
+
+#Calculate Using Simpsons
+
+thing.simp <- chart.test1 %>% ungroup () %>%
+  group_by(Funding.Department.Name) %>%
+  filter(n()>1) %>%
+summarize(test=Bolstad2::sintegral(pctVendor,pctFund)$int)
+
+
