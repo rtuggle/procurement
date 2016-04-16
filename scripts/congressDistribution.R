a148f4eceb98c5a096d2e5a7a1102cac245f5fb7
J.B <jessica.k.bonnie@gmail.com>
2016-04-15 21:31:07 -0400

    renamed variables for clarity

--- scripts/congressDistribution.R
+++ scripts/congressDistribution.R
@@ -1,7 +1,7 @@
 ##prep data for congressional analysis
-require(dplyr)
 require(ggplot2)
-require(flux)
+require(gridExtra)
+require(dplyr)
 
 
 #create a unique key for awards and districts, select needed variables
@@ -10,7 +10,8 @@ congress.frame <- fpds %>%
            Funding.Department.Name, PIID.Agency.ID, PIID, Referenced.IDV.Agency.ID, 
            Referenced..IDV.PIID, Modification.Number, 
            Principal.Place.of.Performance.State.Code,
-           Congressional.District.Place.of..Performance) 
+           Congressional.District.Place.of..Performance,
+           Contracting.Office.ID,Contracting.Office.ID) 
 
 #for reference count how many distinct unique ids
 cntAwards <- n_distinct(congress.frame$uniqueId)
@@ -18,16 +19,16 @@ paste('the number of distinct awards in dataset is:',
       prettyNum(cntAwards, big.mark = ',', scientific = FALSE))
 
 #remove records with NA in CongressId
-noNa <- congress.frame %>%
+congressNoNa <- congress.frame %>%
     filter(!grepl("NA", congressId))
 
-#compare the full and noNa datasets (TASK: bind together to make table)
+#compare the full and congressNoNa datasets (TASK: bind together to make table)
 summarize(congress.frame, distinct = n_distinct(uniqueId), dollars = sum(Action.Obligation))
-summarize(noNa, distinct = n_distinct(uniqueId), dollars = sum(Action.Obligation))
+summarize(congressNoNa, distinct = n_distinct(uniqueId), dollars = sum(Action.Obligation))
 
 #look for remaining records with more than one district
 #this is not meant to happen according to data model
-test <- noNa %>%
+test <- congressNoNa %>%
     group_by(uniqueId) %>%
     summarize(districts = n_distinct(congressId),
               list = paste(congressId, 
@@ -39,17 +40,18 @@ test <- noNa %>%
 write.csv(test, file = "~/Repositories/data/exceptionsCongress.csv", row.names = FALSE)
 
 #get the total spending by Funding Department, rank, and get percentages
-chart <- noNa %>%
+congress.funding <- congressNoNa %>%
     group_by(Funding.Department.Name, congressId) %>%
     summarize(total = sum(Action.Obligation)) %>%
     arrange(Funding.Department.Name, desc(total)) %>%
     group_by(Funding.Department.Name) %>%
-    mutate(rank = row_number(), pctFund = (cumsum(total) / sum(total)) * 100, 
-           #pctDistrict = cumsum(rank) / sum(rank))
-        pctDistrict = cumsum(rank) / 435)
+    mutate(rank = row_number(), 
+           pctFund = (cumsum(total) / sum(total)) * 100,
+           pctDistrict = (cumsum(rank) / sum(rank)))
 
-#chart the results
-ggplot(data = filter(chart, grepl("DEPARTMENT",Funding.Department.Name)),
+
+#graph the results
+ggplot(data = filter(congress.funding, grepl("DEPARTMENT",Funding.Department.Name)),
        aes(x = pctDistrict, y = pctFund, 
            group = Funding.Department.Name,
            colour = Funding.Department.Name)) +
@@ -57,7 +59,7 @@ ggplot(data = filter(chart, grepl("DEPARTMENT",Funding.Department.Name)),
     scale_x_log10()
 
 
-ggplot(data = filter(chart, !grepl("DEPARTMENT",Funding.Department.Name)),
+ggplot(data = filter(congress.funding, !grepl("DEPARTMENT",Funding.Department.Name)),
        aes(x = pctDistrict, y = pctFund, 
            group = Funding.Department.Name,
            colour = Funding.Department.Name)) +
@@ -65,10 +67,7 @@ ggplot(data = filter(chart, !grepl("DEPARTMENT",Funding.Department.Name)),
 
 
 #output data for Tableau
-write.csv(chart, file = "~/Repositories/data/gsaFundCongress_v15APR16.csv", na = "", row.names = FALSE)
+write.csv(congress.funding, file = "~/Repositories/data/gsaFundCongress_v15APR16.csv", na = "", row.names = FALSE)
 
 
-# calculate area under curve
-
-auc(x, y, thresh = NULL, dens = 100)
 
